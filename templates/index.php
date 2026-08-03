c25c141f0721e9be35daa28a38c33ac4bf926978
Kevin Soh <91311083+KevinSoh1@users.noreply.github.com>
2026-08-03 15:38:09 +0900

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -10,9 +10,8 @@ $user_calendar = "양력";
 
 // 로그인한 사용자이면 DB에서 회원정보 조회
 if (isset($_SESSION['user_email'])) {
-    include "db_conn.php"; // DB 접속 파일
-    
-    $user_email = $_SESSION['user_email'];
+
+	$user_email = $_SESSION['user_email'];
     $user_email = mysqli_real_escape_string($conn, $user_email);
     
     $user_type = $_SESSION['user_type'];
