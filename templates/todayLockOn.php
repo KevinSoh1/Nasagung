610db8d99dab599827c7b1ebae17fbbf7410c8ff
Kevin Soh <91311083+KevinSoh1@users.noreply.github.com>
2026-08-05 14:07:07 +0900

    Delete templates directory

--- templates/todayLockOn.php
+++
@@ -1,9 +0,0 @@
- 	<aside class="quick-menu">
-		<div class="quick-item">
-			<div class="q-icon">??</div>
-				<div class="q-text">
-					<strong>오늘의 운세</strong>
-					<span>오늘의 흐름을 확인해보세요</span>
-				</div>
-        	</div>
-        </aside>
